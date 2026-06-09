bcc216c040862a9f1e78bf1e998b65b3ce001bdf
kzoo <>
2026-06-09 21:52:46 +0900

    add: enhance home page with user input label and navigation logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { pageStyles } from "./page.styles";
 import { useUser } from "./context/UserContext";
 
 export default function Home() {
-  const router = useRouter();
+  const [shouldNavigate, setShouldNavigate] = useState(false); // 画面へ遷移するかどうか
 
+  const router = useRouter();
   const { userName, setUserName, setStartTime } = useUser();
 
   const handleStart = () => {
@@ -15,14 +17,24 @@ export default function Home() {
     setStartTime(Date.now());
 
     // ゲーム画面へ遷移
-    router.push("/game");
+    setShouldNavigate(true);
   };
+
+  useEffect(() => {
+    if (shouldNavigate) {
+      router.push("/game");
+    }
+  }, [shouldNavigate, router]); // shouldNavigateが変更されたときに実行
 
   return (
     <main className={pageStyles.start.main}>
       <div className={pageStyles.start.container}>
         <h1>タイピングゲームへようこそ！</h1>
+        <label htmlFor="userName" className={pageStyles.start.label}>
+          名前
+        </label>
         <input
+          id="userName"
           type="text"
           placeholder="ここに名前を入力"
           className={pageStyles.start.input}
